a1521053392b45bafc4e63404518ac199555a042
yxngxr093 <yxngxr093@gmail.com>
2025-01-20 17:43:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,27 +27,3 @@ async function transliterateTextAutoDetect(outputScript, inputText){
 
     document.getElementById('TransliteratedText').value = data;
 }
-
-
-
-
-
-
-// document.getElementById('TransliterateButton').addEventListener('click', function(){
-//     const inputText = document.getElementById('inputText').value;
-//     const inputScript = document.getElementById('inputScript').value;
-//     const outputScript = document.getElementById('outputScript').value;
-
-//     transliterateText(inputText, inputScript, outputScript);
-// });
-
-// async function transliterateText(inputText, inputScript, outputScript){
-//     const apiURL = `https://api.aksharamukha.org/transliterate?from=${inputScript}&to=${outputScript}&text=${encodeURIComponent(inputText)}`;
-
-//     const response = await fetch(apiURL);
-//     const data = await response.json();
-
-//     if(data && data.transliterated){
-//         document.getElementById('TransliteratedText').value = data.transliterated;
-//     }
-// }
